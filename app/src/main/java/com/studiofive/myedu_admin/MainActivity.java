bc6ba1610704c7be939eb95eb229bd4555c3426a
Samuel-dot-cloud <successam336@gmail.com>
2020-10-28 12:55:40 +0300

    Instantiating the app.

---
+++ app/src/main/java/com/studiofive/myedu_admin/MainActivity.java
@@ -0,0 +1,97 @@
+package com.studiofive.myedu_admin;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import es.dmoral.toasty.Toasty;
+
+public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.inputEmailLogin)
+    EditText mEmailLogin;
+    @BindView(R.id.inputPasswordLogin)
+    EditText mPasswordLogin;
+    @BindView(R.id.btnLogin)
+    Button mBtnLogin;
+
+    private FirebaseAuth mAuth;
+    private ProgressDialog mProgressDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mAuth = FirebaseAuth.getInstance();
+        mProgressDialog = new ProgressDialog(this);
+
+
+        initActionClick();
+    }
+
+    private void initActionClick() {
+        mBtnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LoginDefault();
+            }
+        });
+    }
+
+    private void LoginDefault() {
+        String email = mEmailLogin.getText().toString();
+        String password = mPasswordLogin.getText().toString();
+        if (TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
+            Toasty.warning(this, "Please enter valid details!!", Toast.LENGTH_SHORT, true).show();
+        } else {
+            mProgressDialog.setTitle("Logging In");
+            mProgressDialog.setMessage("Please wait...");
+            mProgressDialog.setCanceledOnTouchOutside(true);
+            mProgressDialog.show();
+
+            mAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                mProgressDialog.dismiss();
+                                Toasty.success(MainActivity.this, "Logged in Successfully!!", Toast.LENGTH_SHORT, true).show();
+                                sendUserToCategoryActivity();
+                            } else {
+                                String message = task.getException().toString();
+                                Toasty.error(MainActivity.this, "Error: " + message, Toast.LENGTH_SHORT, true).show();
+                            }
+                            mProgressDialog.dismiss();
+                        }
+                    });
+        }
+    }
+
+    private void sendUserToCategoryActivity() {
+        Intent intent = new Intent(MainActivity.this, CategoryActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
+    }
+
+}
