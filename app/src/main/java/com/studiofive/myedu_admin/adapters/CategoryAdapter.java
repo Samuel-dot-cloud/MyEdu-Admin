d1f26f60bdc220cc4b2c5c064c1407df0f2a42bd
Samuel-dot-cloud <successam336@gmail.com>
2020-10-29 20:01:31 +0300

    Adding logic for updating categories

--- app/src/main/java/com/studiofive/myedu_admin/adapters/CategoryAdapter.java
+++ app/src/main/java/com/studiofive/myedu_admin/adapters/CategoryAdapter.java
@@ -8,6 +8,8 @@ import android.util.ArrayMap;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -66,6 +68,12 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.ViewHo
         ImageView categoryDelete;
 
         private Dialog loadingDialog;
+        private Dialog editDialog;
+
+        private EditText editCategoryName;
+        private Button editCategoryButton;
+
+        private FirebaseFirestore mFirestore;
 
 
         public ViewHolder(@NonNull View itemView) {
@@ -77,10 +85,40 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.ViewHo
             loadingDialog.setCancelable(false);
             loadingDialog.getWindow().setBackgroundDrawableResource(R.drawable.progress_background);
             loadingDialog.getWindow().setLayout(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+
+            editDialog = new Dialog(itemView.getContext());
+            editDialog.setContentView(R.layout.edit_category_dialog);
+            editDialog.setCancelable(true);
+            editDialog.getWindow().setLayout(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+
+            editCategoryName = editDialog.findViewById(R.id.updatecategoryNameEditText);
+            editCategoryButton = editDialog.findViewById(R.id.updateCategoryButtonDialog);
         }
 
-        private void setData(String title, int position, CategoryAdapter adapter){
+        private void setData(String title, int position, CategoryAdapter adapter) {
             categoryName.setText(title);
+
+            itemView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    editCategoryName.setText(categoryList.get(position).getName());
+
+                    editDialog.show();
+                    return false;
+                }
+            });
+
+            editCategoryButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (editCategoryName.getText().toString().isEmpty()) {
+                        editCategoryName.setError("Enter category name!");
+                        return;
+                    }
+
+                    updateCategory(editCategoryName.getText().toString(), position, itemView.getContext(), adapter);
+                }
+            });
 
             categoryDelete.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -102,7 +140,7 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.ViewHo
                     dialog.getButton(dialog.BUTTON_NEGATIVE).setBackgroundColor(Color.GREEN);
 
                     LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-                    params.setMargins(0, 0 ,50, 0);
+                    params.setMargins(0, 0, 50, 0);
                     dialog.getButton(dialog.BUTTON_NEGATIVE).setLayoutParams(params);
                 }
             });
@@ -111,12 +149,12 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.ViewHo
         private void deleteCategory(final int id, Context mContext, CategoryAdapter adapter) {
             loadingDialog.show();
 
-            FirebaseFirestore mFirestore = FirebaseFirestore.getInstance();
+            mFirestore = FirebaseFirestore.getInstance();
 
             Map<String, Object> categoryDoc = new ArrayMap<>();
             int index = 1;
-            for (int i = 0; i < categoryList.size(); i++){
-                if (i != id){
+            for (int i = 0; i < categoryList.size(); i++) {
+                if (i != id) {
                     categoryDoc.put("Cat" + String.valueOf(index) + "_ID", categoryList.get(i).getId());
                     categoryDoc.put("Cat" + String.valueOf(index) + "_Name", categoryList.get(i).getName());
                     index++;
@@ -137,12 +175,56 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.ViewHo
                         }
                     })
                     .addOnFailureListener(new OnFailureListener() {
-                @Override
-                public void onFailure(@NonNull Exception e) {
-                    loadingDialog.dismiss();
-                    Toasty.success(mContext.getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT, true).show();
-                }
-            });
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            loadingDialog.dismiss();
+                            Toasty.error(mContext.getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT, true).show();
+                        }
+                    });
+        }
+
+        private void updateCategory(String newName, int position, Context mContext, CategoryAdapter adapter) {
+            editDialog.dismiss();
+            loadingDialog.show();
+
+            Map<String, Object> categoryData = new ArrayMap<>();
+            mFirestore = FirebaseFirestore.getInstance();
+            mFirestore.collection("PreQuiz").document(categoryList.get(position).getId())
+                    .update(categoryData)
+                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            Map<String, Object> categoryDoc = new ArrayMap<>();
+                            categoryDoc.put("Cat" + String.valueOf(position + 1) + "_Name", newName);
+
+                            mFirestore.collection("PreQuiz").document("Categories")
+                                    .update(categoryDoc)
+                                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                        @Override
+                                        public void onSuccess(Void aVoid) {
+                                            Toasty.success(mContext.getApplicationContext(), "Category name updated successfully", Toast.LENGTH_SHORT, true).show();
+                                            CategoryActivity.categoryList.get(position).setName(newName);
+                                            adapter.notifyDataSetChanged();
+                                            loadingDialog.dismiss();
+                                        }
+                                    })
+                                    .addOnFailureListener(new OnFailureListener() {
+                                        @Override
+                                        public void onFailure(@NonNull Exception e) {
+                                            Toasty.error(mContext.getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT, true).show();
+                                        }
+                                    });
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            loadingDialog.dismiss();
+                            Toasty.error(mContext.getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT, true).show();
+
+                        }
+                    });
+
         }
     }
 }
