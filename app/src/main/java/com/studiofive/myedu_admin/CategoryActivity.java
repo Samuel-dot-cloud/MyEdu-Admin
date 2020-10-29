2cdc9221424482215f785a236efd2ccc123ee354
Samuel-dot-cloud <successam336@gmail.com>
2020-10-29 14:00:29 +0300

    Designing the category layout and adapter

--- app/src/main/java/com/studiofive/myedu_admin/CategoryActivity.java
+++ app/src/main/java/com/studiofive/myedu_admin/CategoryActivity.java
@@ -1,14 +1,30 @@
 package com.studiofive.myedu_admin;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class CategoryActivity extends AppCompatActivity {
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.categoryRecyclerView)
+    RecyclerView categoryRecyclerview;
+    @BindView(R.id.addCategoryButton)
+    Button categoryButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category);
+        ButterKnife.bind(this);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Categories");
     }
 }
